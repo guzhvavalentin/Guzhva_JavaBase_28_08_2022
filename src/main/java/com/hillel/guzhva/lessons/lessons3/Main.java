665d44ce1d99c00c6423d922f9aaf8b6abcf70e8
Guzhva Valentin <guzhvavalentin@gmail.com>
2022-09-06 23:15:10 +0300

    lesson3 paralleled

--- src/main/java/com/hillel/guzhva/lessons/lessons3/Main.java
+++ src/main/java/com/hillel/guzhva/lessons/lessons3/Main.java
@@ -2,6 +2,13 @@ package com.hillel.guzhva.lessons.lessons3;
 
 public class Main {
     public static void main(String[] args) {
+        int a = 5;
+        int b = 5;
+        int h = 5;
+        int volume = 5*5*5;
+        System.out.println("Volume Parallelepiped = " + volume);
+
+
 
 
     }
