95329da456c584ef551d39d8e2fa2a667621b5a7
Guzhva Valentin <guzhvavalentin@gmail.com>
2022-09-06 23:26:04 +0300

    lesson3 paralleled

--- src/main/java/com/hillel/guzhva/lessons/lessons3/Main.java
+++ src/main/java/com/hillel/guzhva/lessons/lessons3/Main.java
@@ -8,6 +8,15 @@ public class Main {
         int volume = 5*5*5;
         System.out.println("Volume Parallelepiped = " + volume);
 
+        System.out.println();
+
+        int a1 = 5;
+        int b1 = 5;
+        int c1 = 5;
+        int length =4*(a1+b1+c1);
+        System.out.println("Amount Length Parallelepiped = " + length);
+
+
 
 
 
