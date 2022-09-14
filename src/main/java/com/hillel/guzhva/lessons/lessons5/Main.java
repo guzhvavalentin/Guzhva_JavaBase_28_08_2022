ebd0e84660bf2be33791bdbeabde518793349b97
Guzhva Valentin <guzhvavalentin@gmail.com>
2022-09-14 22:16:14 +0300

    lesson 5 counter strike 3.0

--- src/main/java/com/hillel/guzhva/lessons/lessons5/Main.java
+++ src/main/java/com/hillel/guzhva/lessons/lessons5/Main.java
@@ -3,6 +3,7 @@ package com.hillel.guzhva.lessons.lessons5;
 public class Main {
     public static void main(String[] args) {
 
+        String team = "Black";
         int gamer1 = 9;
         int gamer2 = 9;
         int gamer3 = 10;
@@ -11,17 +12,19 @@ public class Main {
         final int a = 5;
         int result1 = (gamer1+gamer2+gamer3+gamer4+gamer5) / a;
 
-        int shooter1 = 4;
-        int shooter2 = 8;
-        int shooter3 = 7;
-        int shooter4 = 3;
-        int shooter5 = 4;
-        int result2 = (shooter1+shooter2+shooter3+shooter4+shooter5) / a;
+        String team2 = "White";
+        int gamer6 = 4;
+        int gamer7 = 8;
+        int gamer8 = 7;
+        int gamer9 = 3;
+        int gamer10 = 4;
+        int result2 = (gamer6+gamer7+gamer8+gamer9+gamer10) / a;
 
         if (result1 > result2){
-            System.out.println("Пбедила команда Black! Набрала "+ result1 + " очков");
+            System.out.println("Пбедила команда " + team + " с результатом " + result1 + " очков");
         } else if (result1 < result2) {
-            System.out.println("Победила команда: White! Набрала " + result2 + " очков");
+            System.out.println("Победила команда " + team2 + " с результатом " + result2 + " очков");
+
         }
 
 
