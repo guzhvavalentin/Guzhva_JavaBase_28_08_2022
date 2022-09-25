96364207a0a815af1c7baa379417e7980efed959
Guzhva Valentin <guzhvavalentin@gmail.com>
2022-09-25 23:18:44 +0300

    lesson 5 counter strike final

--- src/main/java/com/hillel/guzhva/lessons/lessons5/Main.java
+++ src/main/java/com/hillel/guzhva/lessons/lessons5/Main.java
@@ -12,18 +12,19 @@ public class Main {
         System.out.println(black);
 
         System.out.println("введи количество очков игрока 1 первой команды");
+        int player1 = 0;
         if (scanner.hasNextInt()) {
-            int player1 = scanner.nextInt();
+            player1 = scanner.nextInt();
             System.out.println(player1);
         } else {
             System.out.println("ведены не верные данные");
             System.exit(0);
         }
-        scanner.nextLine();
 
         System.out.println("введи количество очков игрока 2 первой команды");
+        int player2 = 0;
         if (scanner.hasNextInt()) {
-            int player2 = scanner.nextInt();
+            player2 = scanner.nextInt();
             System.out.println(player2);
         } else {
             System.out.println("ведены не верные данные");
@@ -32,8 +33,9 @@ public class Main {
         scanner.nextLine();
 
         System.out.println("введи количество очков игрока 3 первой команды");
+        int player3 = 0;
         if (scanner.hasNextInt()) {
-            int player3 = scanner.nextInt();
+            player3 = scanner.nextInt();
             System.out.println(player3);
         } else {
             System.out.println("ведены не верные данные");
@@ -42,8 +44,9 @@ public class Main {
         scanner.nextLine();
 
         System.out.println("введи количество очков игрока 4 первой команды");
+        int player4 = 0;
         if (scanner.hasNextInt()) {
-            int player4= scanner.nextInt();
+            player4 = scanner.nextInt();
             System.out.println(player4);
         } else {
             System.out.println("ведены не верные данные");
@@ -52,8 +55,9 @@ public class Main {
         scanner.nextLine();
 
         System.out.println("введи количество очков игрока 5 первой команды");
+        int player5 = 0;
         if (scanner.hasNextInt()) {
-            int player5= scanner.nextInt();
+            player5 = scanner.nextInt();
             System.out.println(player5);
         } else {
             System.out.println("ведены не верные данные");
@@ -63,22 +67,22 @@ public class Main {
 
         System.out.println("Введи название второй команды");
         String white = scanner.nextLine();
-        System.out.println(white);
-
+        System.out.println(black);
 
         System.out.println("введи количество очков игрока 1 второй команды");
+        int player6 = 0;
         if (scanner.hasNextInt()) {
-            int player6= scanner.nextInt();
+            player6 = scanner.nextInt();
             System.out.println(player6);
         } else {
             System.out.println("ведены не верные данные");
             System.exit(0);
         }
-        scanner.nextLine();
 
         System.out.println("введи количество очков игрока 2 второй команды");
+        int player7 = 0;
         if (scanner.hasNextInt()) {
-            int player7= scanner.nextInt();
+            player7 = scanner.nextInt();
             System.out.println(player7);
         } else {
             System.out.println("ведены не верные данные");
@@ -87,8 +91,9 @@ public class Main {
         scanner.nextLine();
 
         System.out.println("введи количество очков игрока 3 второй команды");
+        int player8 = 0;
         if (scanner.hasNextInt()) {
-            int player8= scanner.nextInt();
+            player8 = scanner.nextInt();
             System.out.println(player8);
         } else {
             System.out.println("ведены не верные данные");
@@ -97,8 +102,9 @@ public class Main {
         scanner.nextLine();
 
         System.out.println("введи количество очков игрока 4 второй команды");
+        int player9 = 0;
         if (scanner.hasNextInt()) {
-            int player9= scanner.nextInt();
+            player9 = scanner.nextInt();
             System.out.println(player9);
         } else {
             System.out.println("ведены не верные данные");
@@ -107,8 +113,9 @@ public class Main {
         scanner.nextLine();
 
         System.out.println("введи количество очков игрока 5 второй команды");
+        int player10 = 0;
         if (scanner.hasNextInt()) {
-            int player10= scanner.nextInt();
+            player10 = scanner.nextInt();
             System.out.println(player10);
         } else {
             System.out.println("ведены не верные данные");
@@ -116,38 +123,19 @@ public class Main {
         }
         scanner.nextLine();
 
-        System.out.println();
-
-        String team = "Black";
-        int player1 = 9;
-        int player2 = 9;
-        int player3 = 10;
-        int player4 = 11;
-        int player5 = 11;
-
-        final int a = 5;
-        int result1 = (player1 + player2 + player3 + player4 + player5) / a;
+        String team1 = "Black";
+        double result = (player1 + player2 + player3 +player4 + player5) / 5;
 
         String team2 = "White";
-        int player6 = 4;
-        int player7 = 8;
-        int player8 = 7;
-        int player9 = 3;
-        int player10 = 14;
-        int result2 = (player6 + player7 + player8 + player9 + player10) / a;
+        double result2 = (player6 + player7 + player8 +player9 + player10) / 5;
 
-        if (result1 > result2) {
-            System.out.println("Победила команда " + team + " набрала " + result1 + " очков");
-        } else if (result1 < result2) {
-            System.out.println("Победила команда " + team2 + " набрала " + result2 + " очков");
+        if (result > result2) {
+            System.out.println("Победила команда " + team1 + " набрала " + result + " очков" );
+        } else if (result < result2) {
+            System.out.println("Победила команда " + team2 + " набрала " + result2 + " очков" );
+        } else if (result == result2) {
+            System.out.println("Ничья");
         }
-
-
-
-
-
-
-
 
 
     }
